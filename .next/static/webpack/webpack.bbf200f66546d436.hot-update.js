d65315424c66e48508f3552c5a57a347f4a01791
Unmesh28 <unmesh.dabhade@gmail.com>
2022-05-29 07:49:52 +0530

    window varibale not defined error fixed

--- .next/static/webpack/webpack.bbf200f66546d436.hot-update.js
+++ .next/static/webpack/webpack.bbf200f66546d436.hot-update.js
@@ -9,6 +9,11 @@
  */
 self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/amd options */
+/******/ !function() {
+/******/ 	__webpack_require__.amdO = {};
+/******/ }();
+/******/ 
 /******/ /* webpack/runtime/compat get default export */
 /******/ !function() {
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -35,7 +40,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6f3a0eaae8bcf06f"; }
+/******/ 	__webpack_require__.h = function() { return "143a19ec9045beb0"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/make namespace object */
