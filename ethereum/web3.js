14fc3ed51845200f9a3ebe8aa2726633dde40f2e
Unmesh28 <unmesh.dabhade@gmail.com>
2022-05-29 08:19:02 +0530

    updates

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c"
+    "https://rinkeby.infura.io/v3/cbe00bd12f7d4c55ac380833d5c19dae"
   );
   web3 = new Web3(provider);
 }
