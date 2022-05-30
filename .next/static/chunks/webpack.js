3916c9e84475e2f0d9250874bad2a786a6e3c2ef
Unmesh28 <unmesh.dabhade@gmail.com>
2022-05-30 08:52:53 +0530

    Updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5d638378aef2abe3"; }
+/******/ 		__webpack_require__.h = function() { return "33bd29e8531aa478"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/campaigns/show.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react/cjs/react.production.min";
+
+class CampaignShow extends Component {
+    render() {
+        return <h3>Campaign show</h3>;
+    }
+}
+
+export default CampaignShow;
